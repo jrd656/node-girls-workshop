d0220345f6463da7724cc052377005640738130e
James Daniels <jrd656@gmail.com>
2020-04-05 14:23:26 +0300

    5th?

--- server.js
+++ server.js
@@ -5,33 +5,44 @@ var message = 'I am so happy to be part of the Node Girls workshop!';
 
 var server = http.createServer(handler);
 
-function handler (request, response){
-    response.writeHead(200, {"Content-Type" : "text/html"});
-    response.write(message);
-    response.end();
+function handler(request, response) {
+
     var endpoint = request.url;
 
-    if(endpoint === '/'){
-        response.writeHead(200,{"Content-Type": "text/html"})
-
-        fs.readFile(__dirname + '/public/index.html', function(error,file){
-            if(error){
+    if (endpoint === '/') {
+        response.writeHead(200, { "Content-Type": "text/html" })
+        fs.readFile(__dirname + '/public/index.html', function (error, file) {
+            if (error) {
                 console.log(error)
                 return;
             }
             response.end(file)
         })
-    }
+    } else if (endpoint === '/node') {
+        response.writeHead(200, { "Content-Type" : "text/html"})
+        fs.readFile(__dirname + "/public/node.html", function (error,file){
+            if(error){
+                console.log(error)
+                return;
+            }
+        })
+        //...
+    } else if (endpoint === '/girls') {
+        response.writeHead(200, { "Content-Type" : "text/html"})
+        fs.readFile(__dirname + "/public/girls.html", function (error,file){
+            if(error){
+                console.log(error)
+                return;
+            }
+        })
+        //...
+    } else {
 
-    if(request.url === "/node"){
-        console.log("we got node", request.url, request.method)
-    }
-    else if (request.url === "/girls"){
-        console.log("we got girls", request.url, request.method)
+        // TODO - write your generic endpoint code here
     }
 }
 
-server.listen(3000, function(){
+server.listen(3000, function () {
     console.log("Server is listening on port 3000. Ready to accept requests!");
 })
 
