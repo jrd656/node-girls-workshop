84b2cb962acc992b3b2815d43bb27015d35090b3
James Daniels <jrd656@gmail.com>
2020-04-05 15:30:23 +0300

    Just reverted and pasted html

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var fs = require('fs');
 var querystring = require('querystring');
 var handler = require("./src/handler")
 
-var message = 'I am so happy to be part of the Node Girls workshop!';
-
 var server = http.createServer(handler);
 
 server.listen(3000, function () {
