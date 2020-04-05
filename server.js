b74a32976055e8cd5a5d5664f42734fd997f7ab0
James Daniels <jrd656@gmail.com>
2020-04-05 14:10:42 +0300

    3rd step

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var fs = require('fs');
 
 var message = 'I am so happy to be part of the Node Girls workshop!';
 
@@ -8,6 +9,26 @@ function handler (request, response){
     response.writeHead(200, {"Content-Type" : "text/html"});
     response.write(message);
     response.end();
+    var endpoint = request.url;
+
+    if(endpoint === '/'){
+        response.writeHead(200,{"Content-Type": "text/html"})
+
+        fs.readFile(__dirname + '/public/index.html', function(error,file){
+            if(error){
+                console.log(error)
+                return;
+            }
+            response.end(file)
+        })
+    }
+
+    if(request.url === "/node"){
+        console.log("we got node", request.url, request.method)
+    }
+    else if (request.url === "/girls"){
+        console.log("we got girls", request.url, request.method)
+    }
 }
 
 server.listen(3000, function(){
