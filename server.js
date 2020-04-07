d92256d6a2dbbace2a475e508ee46134a1262d8e
James Daniels <jrd656@gmail.com>
2020-04-07 17:11:44 +0300

    not sure where I'm at

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var http = require('http');
-var fs = require('fs');
-var querystring = require('querystring');
-var handler = require("./src/handler")
+
+var message = 'I am so happy to be part of the Node Girls workshop!';
 
 var server = http.createServer(handler);
 
-server.listen(3000, function () {
+function handler (request, response){
+    response.writeHead(200, {"Content-Type" : "text/html"});
+    response.write(message);
+    response.end();
+}
+
+server.listen(3000, function(){
     console.log("Server is listening on port 3000. Ready to accept requests!");
 })
 
--- src/handler.js
+++ src/handler.js
@@ -27,8 +27,15 @@ var handler = function (request, response) {
             }
             response.end(file)
         })
-        // TODO - write your generic endpoint code here
-
+    } else if (endpoint === '/script.js') {
+        response.writeHead(200, { "Content-Type": "application/javascript" })
+        fs.readFile(__dirname + "/../public/script.js", function (error, file) {
+            if (error) {
+                console.log(error)
+                return;
+            }
+            response.end(file)
+        })
         // TODO - write your generic endpoint code here
     } else if (endpoint === '/img/logo1.png') {
         response.writeHead(200, { "Content-Type": "image/apng" })
@@ -53,15 +60,43 @@ var handler = function (request, response) {
         request.on('data', function (chunkOfData) {
             allTheData += chunkOfData;
         })
-
         request.on('end', function () {
             var convertedData = querystring.parse(allTheData);
-            console.log(convertedData)
-            response.writeHead(303, { "Location": "/" })
-            response.end();
+            writePostJSON(convertedData, function () {
+
+                response.writeHead(303, { "Location": "/" })
+                response.end();
+            });
+        });
+    } else if (endpoint === '/posts') {
+        let dataFromReadFile = fs.readFile('./src/posts.json', 'utf-8', (err, data) => {
+            if (err) throw err;
+            console.log(JSON.parse(data));
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+            // response.writeHead(303, { "Location": "/" })
+            response.end(data);
         })
+
+
     }
 
+}
+
+function writePostJSON(postedObject, callback) {
+    let dataFromReadFile = fs.readFile('./src/posts.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        var jsonObject = JSON.parse(data);
+        var currentTime = Date.now()
+        jsonObject[currentTime] = postedObject.post
+        console.log(jsonObject)
+
+        fs.writeFile('./src/posts.json', JSON.stringify(jsonObject), 'utf-8', (err) => {
+            if (err) throw err;
+            console.log('done writefile!')
+            callback();
+        })
+
+    })
 }
 
 module.exports = handler;
